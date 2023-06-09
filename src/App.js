43201fa19453b8739fadf30bf4cf8e58150069a8
quppiehy <sunhuiyi@gmail.com>
2023-06-09 14:03:19 +0800

    Page links working copy

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React from "react";
-import logo from "./logo.png";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+import logo from "./images/Logo.png";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import ForgetPassword from "./Components/ForgetPassword";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: "home",
+    };
+  }
+
+  //to set correct page
+  handlePageChange = (page) => {
+    this.setState({ currentPage: page });
+  };
+
   render() {
+    const { currentPage } = this.state;
+
+    let pageComponent;
+
+    if (currentPage === "home") {
+      pageComponent = <Login page={this.handlePageChange} />;
+    } else if (currentPage === "signup") {
+      pageComponent = <Signup page={this.handlePageChange} />;
+    } else if (currentPage === "forgetpassword") {
+      pageComponent = <ForgetPassword page={this.handlePageChange} />;
+    }
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-        </header>
-      </div>
+      <Container fluid className="gradient">
+        <Row>
+          <Col xs lg xl xxl="12">
+            <img className="logo_padding logo_size" src={logo} alt="Logo" />
+          </Col>
+        </Row>
+        <Row className="justify-items-center">
+          <Col xs="0" md="0.8" lg xl="1" xxl="3"></Col>
+          <Col xs="12" md="6.2" lg xl="5" xxl="4" className="logo_padding">
+            <div className="logo_padding site_title">
+              The
+              <br />
+              StockTaker
+            </div>
+            <br />
+            <div className="site_text">
+              Welcome to the StockTaker. <br />
+              {currentPage === "signup" ? (
+                <div> Please signup for an account</div>
+              ) : currentPage === "forgetpassword" ? (
+                <div>Please key in your details to reset your password</div>
+              ) : (
+                <div>Please sign in to your account.</div>
+              )}
+            </div>
+            <br />
+          </Col>
+          <Col xs="12" md="4" lg xl="3" xxl="2">
+            {pageComponent}
+          </Col>
+          <Col xs md="0" lg xl="1" xxl="3"></Col>
+        </Row>
+      </Container>
     );
   }
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,82 @@
+import React from "react";
+import "../App.css";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Form from "react-bootstrap/Form";
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentPage: "login",
+    };
+  }
+
+  render() {
+    return (
+      <div className="login_rectangle_login">
+        <Row>
+          <Col>
+            <h2>Login</h2>
+          </Col>
+        </Row>
+        <Form>
+          <Row className="login_box_row">
+            <Col>
+              <Form.Group>
+                <Form.Control
+                  className="text_field w-75 mx-auto"
+                  type="text"
+                  placeholder="Username"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="login_box_row2">
+            <Col>
+              <Form.Group>
+                <Form.Control
+                  className="text_field w-75 mx-auto"
+                  type="text"
+                  placeholder="Password"
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row className="login_box_row3">
+            <Col>
+              <button
+                className="align_left button_astext"
+                onClick={() => this.props.page("signup")}
+              >
+                Signup
+              </button>
+            </Col>
+            <Col>
+              <button
+                className="=align_right button_astext_right"
+                onClick={() => this.props.page("forgetpassword")}
+              >
+                Forget Password
+              </button>
+            </Col>
+          </Row>
+          <Row className="login_box_row2">
+            <Col>
+              <br />
+              <button
+                className="main_button mx-auto"
+                onClick={() => this.props.page("login")}
+              >
+                Login
+              </button>
+            </Col>
+          </Row>
+        </Form>
+      </div>
+    );
+  }
+}
+
+export default Login;
